5f3e0cc0473e096e9998e1395012556110f46e9a
elialbert <eli@simplerelevance.com>
2015-09-15 16:51:59 -0500

    cleanup and beginning of skip squares

--- app/home/home.js
+++ app/home/home.js
@@ -3,21 +3,26 @@
 
   var app = angular.module('myApp.home', ['firebase.auth', 'firebase', 'firebase.utils', 'ngRoute','once']);
 
+    var badPaintSquares = new Set([0,1,2,3,4,5,6]); 
+
   app.controller('HomeCtrl', ['$scope', 'fbutil', 'user', '$firebaseObject', 'FBURL', '$window',function ($scope, fbutil, user, $firebaseObject, FBURL, $window) {
     $scope.user = user;
     $scope.FBURL = FBURL;
     $scope.bgWidth = $window.innerWidth;
     $scope.bgHeight = ((1820*$scope.bgWidth) / 1520); // dimensions of crumbleafter2.jpg
-    $scope.fgWidth = Math.round($scope.bgWidth/80);
-    $scope.fgHeight = Math.round($scope.bgHeight/100);
+    $scope.fgWidth = Math.round($scope.bgWidth/80); //80
+    $scope.fgHeight = Math.round($scope.bgHeight/100); //100
     var constructCoordlist = function() {
 	var coordList = []
 	var id = 0;
 	for (var i=0;i<$scope.bgHeight / $scope.fgHeight;i++) {
 	    for (var j=0;j<$scope.bgWidth / $scope.fgWidth;j++) {
 		var entry = {top:i*$scope.fgHeight+'px',left:j*$scope.fgWidth+'px',id:id};
+		if (!badPaintSquares.has(id)) {		   
+		    coordList.push(entry);
+		}
 		id += 1;
-		coordList.push(entry);
+
 	    }
 	}
 	return coordList;
@@ -25,14 +30,8 @@
 
     $scope.coordList = constructCoordlist();//[{top:'10px',left:'10px',id:0},{top:'10px',left:'50px',id:1}];
     console.log("coordlist length is " + $scope.coordList.length);
-    //var prevHash = {};
-    
     _.each($scope.coordList, function(coordObj) { 
-      //$scope["colorHash_"+coordObj.id.toString()] = {top:coordObj.top,left:coordObj.left,color:''};
-	
       var syncedRectColor = $firebaseObject(fbutil.ref('paintsquares/'+coordObj.id.toString()));
-      //prevHash[coordObj.id] = '';	 // init prevhash - used to return from yellow highlight
-      
       syncedRectColor.$loaded().then(function() {
         if (!syncedRectColor.color) { // init fb storage on first run
 	  syncedRectColor.top=coordObj.top;
@@ -43,24 +42,9 @@
 
 	// MAIN BINDTO per square for individual 3way binding
         syncedRectColor.$bindTo($scope,"colorHash_"+coordObj.id.toString())
-	      //.then(function() {
-	  //prevHash[coordObj.id] = getSquare(coordObj.id).color; // reinit prevhash for preexisting
-        //});
-
-
       });
 
     });  
-
-
-    $scope.hoverEnterAction = function(objId) {
-	return
-      getSquare(objId).color = 'yellow'; // sets on screen and in fb due to 3way binding
-    };
-    $scope.hoverLeaveAction = function(objId) {
-	return
-      getSquare(objId).color = prevHash[objId];
-    };
 
     $scope.clickAction2 = function(event) {
 	var objId = event.srcElement.id;
@@ -69,15 +53,9 @@
 
     $scope.clickAction = function(objId) {
       var obj = getSquare(objId)
-      /*
-      if (prevHash[objId] == "red") {
-        obj.color = "";
+      if (!obj) {
+	  return // maybe display something briefly?
       }
-      else {
-        obj.color = "red";
-      }
-      prevHash[objId] = obj.color;
-      */
       if (obj.color == "") {
 	  obj.color = "red";
       }
@@ -92,7 +70,6 @@
 	    return obj.color;
 	}
     }
-
     var getSquare = function(objId) {
 	return $scope['colorHash_'+objId.toString()];
 	//return $scope[objId]
